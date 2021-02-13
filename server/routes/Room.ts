d7abba90aa8fa674d1508b8c80900f708f568689
Ramko9999 <ramapitchala@gmail.com>
2021-02-13 13:00:20 -0600

    Got Redis to work

--- server/routes/Room.ts
+++ server/routes/Room.ts
@@ -1,6 +1,6 @@
-import {randomBytes} from "crypto";
-import {redisClient} from "../core/HttpServer";
-import {Room} from "../core/Room";
+import { randomBytes } from "crypto";
+import { redisClient } from "../core/HttpServer";
+import { Room, roomPool } from "../core/Room";
 import express = require("express");
 
 
@@ -8,21 +8,27 @@ const roomRouter = express.Router();
 
 roomRouter.post("/", async (req, res) => {
     const roomId = randomBytes(12).toString("hex");
-    res.send({roomId: roomId});
+    res.send({ roomId: roomId });
 });
 
 roomRouter.get("/:id", async (req, res) => {
     const id = req.params.id;
     redisClient.get(id, (err, result) => {
-        if(err !== null || result === null){
+        if (err !== null || result === null) {
             res.status(404).send({
-                message: err?.message
+                message: err?.message,
+                rooms: Array.from(roomPool)
             });
         }
-
-        const room = Room.from(JSON.parse(result as string));
-        res.send(room.serialize());
+        else {
+            const room = Room.from(JSON.parse(result as string));
+            res.send({
+                room: room,
+                rooms: Array.from(roomPool)
+            });
+        }
     });
 });
+
 
 export default roomRouter;
